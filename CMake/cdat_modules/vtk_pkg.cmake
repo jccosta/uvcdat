843f2eef477452d5932e138f5d54789800110fbc
Dan Lipsa <dan.lipsa@kitware.com>
2016-02-18 23:05:02 -0500

    New VTK for testing

--- CMake/cdat_modules/vtk_pkg.cmake
+++ CMake/cdat_modules/vtk_pkg.cmake
@@ -1,4 +1,4 @@
 set(VTK_SOURCE ${GIT_PROTOCOL}github.com/UV-CDAT/VTK.git )
 set(VTK_MD5)
-set(VTK_BRANCH update_vtk_proj4_9_2)
+set(VTK_BRANCH fix_macos_inconsistency)
 add_cdat_package_dependent(VTK "" "" ON "CDAT_BUILD_GRAPHICS" OFF)
